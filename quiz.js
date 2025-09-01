24929ba91f40a8ec33eb0349bf6a9cd6696dcd2d
Sathyavarshinib <sathyavarshini1403@gmail.com>
2025-09-01 16:18:29 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,13 +1,16 @@
 document.getElementById("quizForm").addEventListener("submit", function(e) {
   e.preventDefault();
+
   let subject = document.getElementById("subject").value;
-  let marks = document.getElementById("marks").value;
+  let marks = parseInt(document.getElementById("marks").value); // 👉 number-ஆ convert
   let state = document.getElementById("state").value;
+
   let student = {
     subject: subject,
     marks: marks,
     state: state
   };
+
   localStorage.setItem("studentData", JSON.stringify(student));
   window.location.href = "result.html";
 });
