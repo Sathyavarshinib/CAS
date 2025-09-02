28ffdf67daba6c767e12c52c4f597178e1a8a2db
Sathyavarshinib <sathyavarshini1403@gmail.com>
2025-09-02 12:05:17 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -5,7 +5,7 @@ document.getElementById("quizForm").addEventListener("submit", function(e) {
   const marks = document.getElementById("marks").value;
   const state = document.getElementById("state").value;
   const city = document.getElementById("city").value;
-  const studentData = { subject, marks, state };
+  const studentData = { subject, marks, state, city};
   localStorage.setItem("studentData", JSON.stringify(studentData));
 
   // redirect to result page
